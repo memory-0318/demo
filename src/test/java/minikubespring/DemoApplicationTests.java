45b9370541701c644d4160edad081cfa8720ce06
Brian Su <brian.su@tpisoftware.com>
2020-12-30 01:49:51 +0800

    Initial commit

---
+++ src/test/java/minikubespring/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package minikubespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
